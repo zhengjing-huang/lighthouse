b44fdf50667814c0e686e737b0bc8526a5e041f0
Paul Irish <paulirish@google.com>
2020-12-04 01:57:07 -0800

    move tests

--- lighthouse-core/test/audits/is-on-https-test.js
+++ lighthouse-core/test/audits/is-on-https-test.js
@@ -89,40 +89,4 @@ describe('Security: HTTPS audit', () => {
 
     expect(result.score).toBe(0);
   });
-
-  describe('#isSecureRequest', () => {
-    it('correctly identifies insecure records', () => {
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'http', host: 'google.com'}}),
-        false);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'http', host: '54.33.21.23'}}),
-        false);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'ws', host: 'my-service.com'}}),
-        false);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: '', host: 'google.com'}}),
-        false);
-    });
-
-    it('correctly identifies secure records', () => {
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'http', host: 'localhost'}}),
-        true);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'https', host: 'google.com'}}),
-        true);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'wss', host: 'my-service.com'}}),
-        true);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'data', host: ''}}),
-        true);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'blob', host: ''}}),
-        true);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'filesystem', host: ''}}),
-        true);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'about', host: ''}}),
-        true);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: '', host: ''},
-        protocol: 'blob'}), true);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'chrome', host: ''}}),
-        true);
-      assert.strictEqual(Audit.isSecureRequest({parsedURL: {scheme: 'chrome-extension', host: ''}}),
-        true);
-    });
-  });
 });
